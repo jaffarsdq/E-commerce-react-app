9daa518a2100853107822036dbb06b63230726f2
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-08-15 14:03:38 +0530

    added product tile

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -3,32 +3,37 @@ import ProductBox from "../../components/productBox/ProductBox";
 import reactImg from '../../assets/react.svg'
 //css import
 import './productList.css'
+import ProductTile from "../../components/productTile/ProductTile";
 
 function ProductList() {
     return (
-        <div className="main container  mt-5">
-           <div class="row search-products-wrapper">
-                <FilterProducts/>    
-           </div>
-            <div class="row col-12  col-md-10 col-lg-9 product-list-box d-flex flex-wrap mx-auto" id="product-list">
+        <>
+           <ProductTile/>
+            <div className="main container">
+                <div class="row  col-12  col-md-12 col-lg-3 search-products-wrapper mx-auto">
+                        <FilterProducts/>    
+                </div>
+                <div class="row col-12  col-md-10 col-lg-9 product-list-box d-flex flex-wrap mx-auto mt-4" id="product-list">
 
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
-                <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                    <ProductBox productImage={reactImg} name={'dummy'} price={100}/>
+                </div>
             </div>
-        </div>
+        </>
+        
     )
 }
 
