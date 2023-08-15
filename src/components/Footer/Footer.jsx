dd9baba8e649abaee3e074b543b13244ae592a38
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-08-15 14:11:21 +0530

    updated links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -32,14 +32,14 @@ function Footer() {
           &#169; Jaffar Sadhiq
         </div>
         <div className='social'>
-          <a className='github link' href='https://github.com/jaffarsdq'>
+          <a className='github link' target='_blank' href='https://github.com/jaffarsdq/E-commerce-react-app.git'>
             <i className='bi bi-github'></i>
           </a>
-          <a className='linkedIn link' href='https://www.linkedin.com/in/jaffar-sadhiq/'>
+          <a className='linkedIn link' target='_blank' href='https://www.linkedin.com/in/jaffar-sadhiq/'>
             <i className='bi bi-linkedin'></i>
           </a>
         </div>
-          <a className='mail link' href='mailto:er.jaffarsdq@gmail.com'>
+          <a className='mail link' target='_blank' href='mailto:er.jaffarsdq@gmail.com'>
             <i className='bi bi-envelope-at'></i> er.jaffarsdq@gmail.com
           </a>
       </footer>
