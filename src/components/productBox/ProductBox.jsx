dab10f24865778ecb2fb2969e4c346683163437f
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-08-18 18:28:30 +0530

    upadated product details page to display product details by fetching api

--- src/components/productBox/ProductBox.jsx
+++ src/components/productBox/ProductBox.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 //css import
 import './productBox.css'
 
-function ProductBox({productImage, name, price}) {
+function ProductBox({productImage, name, price, id}) {
   return (      
     
-      <Link  to={`/products/${name}`}  className="container col-9 col-sm-6 col-md-5 col-lg-4 col-xl-3 text-center">
+      <Link  to={`/products/${id}`}  className="container col-9 col-sm-6 col-md-5 col-lg-4 col-xl-3 text-center">
             <div className="col-12  p-2 card ">
                 <div className="col-12 d-flex justify-content-center">
                     <img className="img-fluid" src={productImage} alt="product image"/>
