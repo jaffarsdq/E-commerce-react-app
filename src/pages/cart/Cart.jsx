51a4534594fb51da43afc8e272be1124cff231ed
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-08-30 16:58:02 +0530

    added import

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import CartContext from '../../context/CartContext';
 import axios from 'axios';
 import { getProduct } from '../../apis/fakeStoreApi';
