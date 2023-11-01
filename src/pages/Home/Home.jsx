da98a803e366d2218102fc1e7905b4d217ddd3e3
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-11-01 20:06:08 +0530

    added loading msg

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
 
             <div className="row col-lg-8 col-xl-7 product-list d-flex justify-content-center" 
                 id="categories-list">
-                    {categories ? <div>Pls wait for the initial push , it only takes 2-3 minutes</div> : null}
+                    {categories ? null :<div className='d-flex justify-content-center'> Please wait for the initial push , it only takes 2-3 minutes..</div> }
                 {(categories) ?  <CategoryItem itemName={'All products'}/> : null}
                 {(categories) ? categories.map((category) => 
                     <CategoryItem 
